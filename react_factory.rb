1a6df2bde9974acd80b9f6bb2db615c43e9812f6
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-10 01:49:59 +1000

    components folder and homepage show project name

--- react_factory.rb
+++ react_factory.rb
@@ -1,14 +1,34 @@
 `cp -r template Projects/`
 print 'Enter React project name: '
-project_name = gets.chomp
-`mv Projects/template Projects/#{project_name}`
+app_name = gets.chomp
+`mv Projects/template Projects/#{app_name}`
 
-text = File.read("Projects/#{project_name}/package.json")
-new_contents = text.gsub('template', project_name.to_s)
-File.open("Projects/#{project_name}/package.json", 'w') { |file| file.puts new_contents }
+text = File.read("Projects/#{app_name}/package.json")
+new_contents = text.gsub('template', app_name.to_s)
+File.open("Projects/#{app_name}/package.json", 'w') { |file| file.puts new_contents }
 
-text = File.read("Projects/#{project_name}/package-lock.json")
-new_contents = text.gsub('template', project_name.to_s)
-File.open("Projects/#{project_name}/package-lock.json", 'w') { |file| file.puts new_contents }
+text = File.read("Projects/#{app_name}/package-lock.json")
+new_contents = text.gsub('template', app_name.to_s)
+File.open("Projects/#{app_name}/package-lock.json", 'w') { |file| file.puts new_contents }
 
-`cd Projects/#{project_name} && npm start`
+`mkdir Projects/#{app_name}/src/components`
+`mv Projects/#{app_name}/src/App.js Projects/#{app_name}/src/components`
+
+text = File.read("Projects/#{app_name}/src/index.js")
+new_contents = text.gsub('./App', './components/App')
+File.open("Projects/#{app_name}/src/index.js", 'w') { |file| file.puts new_contents }
+
+new_contents = "
+function App() {
+  return (
+    <div className='App'>
+     <h1>Hello React #{app_name}</h1>
+    </div>
+  );
+}
+
+export default App;
+"
+File.open("Projects/#{app_name}/src/components/App.js", 'w') { |file| file.puts new_contents }
+
+`cd Projects/#{app_name} && npm start`
